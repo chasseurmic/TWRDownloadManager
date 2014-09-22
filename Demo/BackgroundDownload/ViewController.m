4c0fd1ff6c4fee4ef2a877706d47d03c34a15376
Michelangelo Chasseur <chasseurmic@gmail.com>
2014-09-22 22:05:14 +0200

    Update demo project

--- Demo/BackgroundDownload/ViewController.m
+++ Demo/BackgroundDownload/ViewController.m
@@ -6,10 +6,23 @@
 //  Copyright (c) 2014 Touchware. All rights reserved.
 //
 
+#define FILE_URL @"http://ovh.net/files/10Mio.dat"
+
 #import "ViewController.h"
 #import <TWRDownloadManager/TWRDownloadManager.h>
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *mainLabel;
+@property (weak, nonatomic) IBOutlet UIProgressView *progressView;
+@property (weak, nonatomic) IBOutlet UIButton *startButton;
+@property (weak, nonatomic) IBOutlet UIButton *cancelButton;
+@property (weak, nonatomic) IBOutlet UIButton *deleteButton;
+@property (assign, nonatomic) CGFloat progress;
+
+- (IBAction)startDownload:(id)sender;
+- (IBAction)cancelDownload:(id)sender;
+- (IBAction)deleteFiles:(id)sender;
 
 @end
 
@@ -17,13 +30,59 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.progressView.progress = 0.0f;
+    self.mainLabel.text = @"TWRDownloadManager Demo";
+    if ([[TWRDownloadManager sharedManager] fileExistsForUrl:FILE_URL]) {
+        self.deleteButton.enabled = YES;
+        self.cancelButton.enabled = NO;
+        self.startButton.enabled = NO;
+    } else {
+        self.deleteButton.enabled = NO;
+        self.cancelButton.enabled = NO;
+        self.startButton.enabled = YES;
+    }
+}
 
+- (IBAction)startDownload:(id)sender {
     // Just a demo example file...
-    [[TWRDownloadManager sharedManager] downloadFileForURL:@"http://ovh.net/files/10Mio.dat" progressBlock:^(CGFloat progress) {
-        NSLog(@"%.2f", progress);
+    [[TWRDownloadManager sharedManager] downloadFileForURL:FILE_URL progressBlock:^(CGFloat progress) {
+//        NSLog(@"%.2f", progress);
+        self.progress = progress;
+        self.progressView.progress = progress;
+    } remainingTime:^(NSUInteger seconds) {
+        NSLog(@"ETA: %lu sec.", (unsigned long)seconds);
+        self.mainLabel.text = [NSString stringWithFormat:@"Progress: %.0f%% - ETA: %lu sec.", self.progress, seconds];
     } completionBlock:^(BOOL completed) {
         NSLog(@"Download completed!");
+        self.deleteButton.enabled = YES;
+        self.cancelButton.enabled = NO;
+        self.startButton.enabled = NO;
     } enableBackgroundMode:YES];
+    
+    self.cancelButton.enabled = YES;
+    self.startButton.enabled = NO;
+}
+
+- (IBAction)cancelDownload:(id)sender {
+    [[TWRDownloadManager sharedManager] cancelAllDownloads];
+    [self nilProgress];
+    self.startButton.enabled = YES;
+    self.cancelButton.enabled = NO;
+    self.deleteButton.enabled = NO;
+}
+
+- (IBAction)deleteFiles:(id)sender {
+    [[TWRDownloadManager sharedManager] deleteFileForUrl:FILE_URL];
+    [self nilProgress];
+    self.deleteButton.enabled = NO;
+    self.startButton.enabled = YES;
+    self.cancelButton.enabled = NO;
+}
+
+- (void)nilProgress {
+    self.progressView.progress = 0.0f;
+    self.progress = 0.0f;
+    self.mainLabel.text = @"TWRDownloadManager Demo";
 }
 
 @end
